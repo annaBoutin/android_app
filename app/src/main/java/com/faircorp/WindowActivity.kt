8f45500c68501bd0cd2e2baaabbee66397f3886d
Anna Boutin <anna.boutin@etu.emse.fr>
2020-11-25 10:21:55 +0100

    Window activity created with back button

--- app/src/main/java/com/faircorp/WindowActivity.kt
+++ app/src/main/java/com/faircorp/WindowActivity.kt
@@ -8,6 +8,7 @@ class WindowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_window)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val param = intent.getStringExtra(WINDOW_NAME_PARAM)
         val windowName = findViewById<TextView>(R.id.txt_window_name_2)
