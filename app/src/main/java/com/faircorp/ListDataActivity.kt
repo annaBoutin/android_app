b9bda8b48ec2501787ee0f708aa4e42f56643174
Anna Boutin <anna.boutin@etu.emse.fr>
2020-11-25 14:13:12 +0100

    List fenêtre OK

--- app/src/main/java/com/faircorp/ListDataActivity.kt
+++ app/src/main/java/com/faircorp/ListDataActivity.kt
@@ -1,11 +1,30 @@
 package com.faircorp
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.faircorp.model.WindowAdapter
+import com.faircorp.model.WindowService
 
 class ListDataActivity : BasicActivity() {
+
+    val windowService = WindowService()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_data)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.list_windows)
+        val adapter = WindowAdapter()
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+
+        adapter.update(windowService.findAll())
     }
+
 }
--- app/src/main/java/com/faircorp/WindowActivity.kt
+++ app/src/main/java/com/faircorp/WindowActivity.kt
@@ -1,6 +1,6 @@
 package com.faircorp
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
 import android.widget.TextView
 
