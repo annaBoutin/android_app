720f3265efa959306fb909126bde34c276f307eb
Anna Boutin <anna.boutin@etu.emse.fr>
2020-11-25 11:01:59 +0100

    Menu created

--- app/src/main/java/com/faircorp/WindowActivity.kt
+++ app/src/main/java/com/faircorp/WindowActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class WindowActivity : AppCompatActivity() {
+class WindowActivity : BasicActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_window)
