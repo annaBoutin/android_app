60d7b731f39467130317c3419225d142c39e1ce1
Anna Boutin <anna.boutin@etu.emse.fr>
2020-12-21 16:19:46 +0100

    Ecran Room OK

--- app/src/main/java/com/faircorp/RoomActivity.kt
+++ app/src/main/java/com/faircorp/RoomActivity.kt
@@ -1,15 +1,22 @@
 package com.faircorp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.faircorp.model.ApiServices
+import com.faircorp.model.OnWindowSelectedListener
+import com.faircorp.model.RoomAdapter
+import com.faircorp.model.WindowAdapter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class RoomActivity : BasicActivity() {
+class RoomActivity : BasicActivity(), OnWindowSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room)
@@ -49,5 +56,35 @@ class RoomActivity : BasicActivity() {
                     }
                 }
         }
+        val recyclerView = findViewById<RecyclerView>(R.id.list_room_windows)
+        val adapter = WindowAdapter(this)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+
+        lifecycleScope.launch(context = Dispatchers.IO) {
+            runCatching { ApiServices().windowsApiService.findByRoomId(id).execute() }
+                .onSuccess {
+                    withContext(context = Dispatchers.Main) {
+                        adapter.update(it.body() ?: emptyList())
+                    }
+                }
+                .onFailure {
+                    withContext(context = Dispatchers.Main) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Error on windows loading $it",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    override fun onWindowSelected(id: Long) {
+        val intent = Intent(this, WindowActivity::class.java).putExtra(WINDOW_NAME_PARAM, id)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/faircorp/model/WindowApiService.kt
+++ app/src/main/java/com/faircorp/model/WindowApiService.kt
@@ -10,4 +10,7 @@ interface WindowApiService {
 
     @GET("windows/{id}")
     fun findById(@Path("id") id: Long): Call<WindowDto>
+
+    @GET("windows/room/{id}")
+    fun findByRoomId(@Path("id") id: Long): Call<List<WindowDto>>
 }
